2a6de6f2c958cb5512809fb50c7573d402aeef1a
VinnieSanchez11 <it.coordinator.samsug@gmail.com>
2020-07-27 18:03:20 -0600

    js completado

--- buscador.php
+++ buscador.php
@@ -1,8 +1,9 @@
 <?php
-  $data_file = fopen("./data-1.json","r");
-  $data_readed = fread($data_file, filesize("./data-1.json"));
-  $arrayPHP = json_decode($data_readed, true);
-  $DatosJS = array();
+  $fileName = "data-1.json";
+  $data_file = fopen($fileName,"r");
+  $data_readed = fread($data_file, filesize($fileName));
+  $dataArray = json_decode($data_readed);
+  $newData = array();
 
   if(isset($_POST["ciudad"]) && isset($_POST["tipo"]) && isset($_POST["from"]) && isset($_POST["to"])){
     $ciudad = $_POST["ciudad"];
@@ -10,10 +11,10 @@
     $from = $_POST["from"];
     $to = $_POST["to"];
 
-    echo json_encode($DatosJS);
+    echo json_encode($newData);
 
   }else if(isset($_POST["todos"])){
-    echo $arrayPHP;
+    echo $data_readed;
   }
   fclose($data_file);
 
